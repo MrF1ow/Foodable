86af22de127aa376cc2a3630fa7330beec89b433
MrF1ow <eflow1280@gmail.com>
2024-11-10 14:32:50 -0800

    Added ApiGateway and Lambda Class Helpers

--- aws-infra/lib/aws-infra-stack.ts
+++ aws-infra/lib/aws-infra-stack.ts
@@ -2,6 +2,9 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+import { ApiGateway } from './ApiGateway';
+import { Lambda } from './Lambda';
+
 export class AwsInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
