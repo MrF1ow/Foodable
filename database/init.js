0db002da1be2d0cdb79cdbd06d1e108c592d7678
Ethan Flow <109869245+MrF1ow@users.noreply.github.com>
2024-11-14 16:44:36 -0800

    Docker Compose is Functional

--- database/init.js
+++ database/init.js
@@ -1,4 +1,22 @@
+db = db.getSiblingDB('foodable_db');
+
+db.createUser({
+    user: 'admin',
+    pwd: '12345',
+    roles: [
+        {
+            role: 'readWrite',
+            db: 'foodable_db'
+        }
+    ]
+})
+
 db.createCollection('users');
 db.createCollection('recipes');
-db.createCollection('gorceryLists');
+db.createCollection('groceryLists');
 db.createCollection('vectors');
+
+db.users.insertOne({
+    username: 'admin',
+    password: '12345',
+});
