36639489ab4aa1ddfc0627b03b1908139456e52b
nickcknelson <nelson6789@gmail.com>
2025-01-17 18:35:14 -0800

    input ui component done

--- frontend/src/app/grocery-list/[[...grocery-list]]/page.tsx
+++ frontend/src/app/grocery-list/[[...grocery-list]]/page.tsx
@@ -12,6 +12,30 @@ import { GeneralHeader } from "@/components/general-header";
 import { grocerySections } from "@/config/grocery-sections";
 import { GroceryAccordion } from "@/components/grocery/grocery-accordion";
 import { InputCard } from "@/components/input-card/input-card";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  FormField,
+  FormItem,
+  FormControl,
+  FormDescription,
+  FormLabel,
+  FormMessage,
+  Form,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Button } from "@/components/ui/button";
+
+import { z } from "zod";
 
 export default function GroceryList() {
   const [items, setItems] = useState(grocerySections.slice(0, 6));
@@ -80,13 +104,110 @@ export default function GroceryList() {
     );
   };
 
+  const formSchema = z.object({
+    itemName: z.string().min(2).max(50),
+    quantity: z.number().min(1).max(100),
+    category: z.string().min(2).max(50),
+  });
+
   const AddItem = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        itemName: "",
+        quantity: 0,
+        category: "",
+      },
+    });
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+      console.log(values);
+    }
+
     return (
-      <InputCard
-        title="Add Item"
-        content={<div>Content</div>}
-        footer={<button>Add</button>}
-      />
+      <Form {...form}>
+        <InputCard
+          title="Add Item"
+          content={
+            <div className="flex flex-col gap-4">
+              <FormField
+                control={form.control}
+                name="itemName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-xl">Item Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter item name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="quantity"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-xl">Quantity</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter quantity" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="category"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-xl">Select Category</FormLabel>
+                    <div className="border rounded shadow-sm p-2">
+                      <FormControl>
+                        <DropdownMenu>
+                          <DropdownMenuTrigger>
+                            {field.value || "Bakery"}
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent>
+                            {[
+                              "Bakery",
+                              "Dairy",
+                              "Produce",
+                              "Meat",
+                              "Pantry",
+                              "Frozen",
+                            ].map((category) => (
+                              <DropdownMenuItem
+                                key={category}
+                                onClick={() => field.onChange(category)}
+                              >
+                                {category}
+                              </DropdownMenuItem>
+                            ))}
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </FormControl>
+                    </div>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          }
+          footer={
+            <div className="flex justify-center ">
+              <Button
+                type="submit"
+                onClick={form.handleSubmit(onSubmit)}
+                className="btn-primary p-8 text-3xl"
+              >
+                Submit
+              </Button>
+            </div>
+          }
+        />
+      </Form>
     );
   };
 
