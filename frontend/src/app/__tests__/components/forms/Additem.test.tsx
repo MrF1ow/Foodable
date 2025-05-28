6233cf59323588661e260be0980dd351ac3b34d6
nickcknelson <nnelson6789@gmail.com>
2025-05-28 16:41:19 -0700

    Preparing for merge

--- frontend/src/app/__tests__/components/forms/Additem.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-describe("Basic integration test", () => {
-  it("should pass a simple math test", () => {
-    expect(2 + 2).toBe(4);
-  });
-});
--- frontend/src/app/__tests__/components/forms/FindPrice.test.tsx
+++
@@ -1,5 +0,0 @@
-describe("Basic integration test", () => {
-  it("should pass a simple math test", () => {
-    expect(2 + 2).toBe(4);
-  });
-});
