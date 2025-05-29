6e5bbfc5a881fc9e17bb3339cdcc86c8b16de10d
Flow <MrF1ow>
2025-05-28 20:09:56 -0700

    Fixed Grocery List Deletion Issue

--- frontend/src/app/api/user/recipes/deleteRecipe.ts
+++ frontend/src/app/api/user/recipes/deleteRecipe.ts
@@ -8,11 +8,13 @@ import { getCurrentUser } from "@/lib/utils/user";
 
 export async function DELETE(req: Request) {
   try {
-    const { userData, error, status } = await getCurrentUser<
-      { _id: ObjectId, savedItems: any }>({
-        _id: 1,
-        savedItems: 1
-      });
+    const { userData, error, status } = await getCurrentUser<{
+      _id: ObjectId;
+      savedItems: any;
+    }>({
+      _id: 1,
+      savedItems: 1,
+    });
 
     if (!userData) {
       return NextResponse.json({ message: error }, { status });
@@ -22,30 +24,29 @@ export async function DELETE(req: Request) {
     if (!isValidObjectId(id)) {
       return NextResponse.json(
         { message: HTTP_RESPONSES.BAD_REQUEST },
-        { status: 400 }
+        { status: 400 },
       );
     }
 
     const db = await getDB();
 
-    const deletedRecipe = await db
-      .collection("recipes")
-      .findOneAndDelete({
-        _id: ObjectId.createFromHexString(id),
-        creatorId: userData._id,
-      });
+    const deletedRecipe = await db.collection("recipes").findOneAndDelete({
+      _id: ObjectId.createFromHexString(id),
+      creatorId: userData._id,
+    });
 
     if (!deletedRecipe) {
       return NextResponse.json(
         { message: HTTP_RESPONSES.NOT_FOUND },
-        { status: 404 }
+        { status: 404 },
       );
     }
 
     // Remove the deleted ID from savedItems.recipes
     const updatedSavedLists =
       userData.savedItems?.recipes?.filter(
-        (list: { _id: string | ObjectId }) => list._id.toString() !== id.toString()
+        (list: { _id: string | ObjectId }) =>
+          list._id.toString() !== id.toString(),
       ) || [];
 
     // Apply the filtered list back to the user profile
@@ -61,22 +62,21 @@ export async function DELETE(req: Request) {
       { _id: userData._id },
       {
         $set: updateFields,
-        $pull: { createdItems: { _id: ObjectId.createFromHexString(id) } } as any
+        $pull: {
+          createdItems: { _id: ObjectId.createFromHexString(id) },
+        } as any,
       },
     );
 
     await deleteVectorEmbedding(ObjectId.createFromHexString(id));
 
-    return NextResponse.json(
-      { message: HTTP_RESPONSES.OK },
-      { status: 200 }
-    );
+    return NextResponse.json({ message: HTTP_RESPONSES.OK }, { status: 200 });
   } catch (error) {
     console.log("Error deleting recipe", error);
 
     return NextResponse.json(
       { message: HTTP_RESPONSES.INTERNAL_SERVER_ERROR },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
